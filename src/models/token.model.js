2ee4040defec0a6b175da24d70c687ebf90e783b
KuldeepGupta <kuldeepswnkumar@gmail.com>
2024-06-19 16:00:53 +0530

    Added Console log

--- src/models/token.model.js
+++ src/models/token.model.js
@@ -1,20 +1,20 @@
-import mongoose, {Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
 
 const tokenSchema = new Schema({
-    token :{
-        type:String,
-        required:true
+    token: {
+        type: String,
+        required: true
     },
-    user_id :{
-        type:Schema.Types.ObjectId,
-        ref:'User',
-        required:true
+    user_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
     },
-    created_at : {
-        type:Date,
-        default:Date.now
+    created_at: {
+        type: Date,
+        default: Date.now()
     }
 });
 
-export const Token = mongoose.model("Token",tokenSchema)
+export const Token = mongoose.model("Token", tokenSchema)
